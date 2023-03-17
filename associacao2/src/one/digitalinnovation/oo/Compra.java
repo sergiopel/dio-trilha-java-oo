87a71aa7b5f2b7eb5eea5d3d7f8d1b5cb9fed9f8
Sergio Pelegrini <serpel@gmail.com>
2023-03-17 13:25:48 -0300

    projeto associacao

---
+++ associacao2/src/one/digitalinnovation/oo/Compra.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.oo;
+
+// Tipo de associação:
+// Comportamental (métodos)
+//   Dependência
+//     o método finalizar depende da classe Cupom
+public class Compra {
+    void finalizar(Cupom cupom){
+
+    }
+}
---
+++ associacao2/src/one/digitalinnovation/oo/Disciplina.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.oo;
+
+
+// Tipo de associação:
+// Estrutural (atributos)
+//   Agregação
+//     Se a disciplina deixar de existir, não necessariamente o aluno deixará de existir também
+public class Disciplina {
+    Aluno aluno;
+}
