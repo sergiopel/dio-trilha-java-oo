84240dda38073e5da83293c74b709b4be1e974dd
Sergio Pelegrini <serpel@gmail.com>
2023-03-17 15:41:55 -0300

    Visibilidades, também conhecido como Modificadores de Acesso.

---
+++ visibilidades/src/one/digitalinnovation/oo/Classe2.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.oo;
+
+public class Classe2 extends Classe1{
+    //Atributos próprios
+
+    void metodo() {
+        // atributo 2 e 3
+
+    }
+
+    //metodo 2 e 3
+}
---
+++ visibilidades/src/one/digitalinnovation/oo/outro/pacote/Classe4.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.oo.outro.pacote;
+
+import one.digitalinnovation.oo.Classe1;
+
+class Classe4 {
+    Classe1 classe1;
+
+    void metodo() {
+        //atributo3
+
+        //metodo3
+
+    }
+
+}
